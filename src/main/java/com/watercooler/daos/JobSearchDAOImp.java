c498d94098e0823f0cfc40b43175605e0e463a44
Jeryl Skinner <skinner.jeryl@gmail.com>
2022-04-18 16:18:25 -0400

    Just to be sure all files are in

--- src/main/java/com/watercooler/daos/JobSearchDAOImp.java
+++ src/main/java/com/watercooler/daos/JobSearchDAOImp.java
@@ -11,17 +11,25 @@ public class JobSearchDAOImp implements JobSearchDAOInt{
 
     @Override
     public List<Job> selectJob(String jobLocation, String jobType) {
+
         try(Connection connection = DatabaseConnection.createConnection()){
             String append = "";
             if (jobLocation.equals("nothing") && !jobType.equals("nothing")) {
-                append = " where job_type = " + jobType;
+                append = " where job_type = " + "'" + jobType + "'";
             } else if (!jobLocation.equals("nothing") && jobType.equals("nothing")){
-                append = " where job_location = " + jobLocation;
+                append = " where job_location = " + "'" + jobLocation + "'";
             } else if (!jobLocation.equals("nothing") && !jobType.equals("nothing")){
-                append = " where job_location = " + jobLocation + " and job_type = " + jobType;
+                append = " where job_location = " + "'" + jobLocation + "'" + " and job_type = " + "'" + jobType + "'";
+            } else if (jobLocation.equals("nothing") && jobType.equals("nothing")){
+                append = ""; // should be better way to do this
             }
+
+//            System.out.println(append);
+//
             String sql = "select * from job_table" + append;
+            System.out.println(sql);
             Statement s = connection.createStatement();
+//            select * from job_table where job_location = 'testLocation4' and job_type = 'Technology'
             s.execute(sql);
             ResultSet rs = s.getResultSet();
             List<Job> joblist = new ArrayList<>();
@@ -33,7 +41,7 @@ public class JobSearchDAOImp implements JobSearchDAOInt{
                         rs.getString("job_description"),
                         rs.getInt("company_id"),
                         rs.getString("job_location"),
-                        rs.getString("company_name)")
+                        rs.getString("company_name")
                 );
                 joblist.add(job);
             }
@@ -43,6 +51,11 @@ public class JobSearchDAOImp implements JobSearchDAOInt{
             e.printStackTrace();
             return null;
         }
+    }
+    public static void main(String[] args){
+        JobSearchDAOImp JSDI = new  JobSearchDAOImp();
+        List<Job> result = JSDI.selectJob("nothing","Technology");
+        System.out.println(result.toString());
     }
 }
 
