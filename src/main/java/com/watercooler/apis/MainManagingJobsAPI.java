ec14cb5d9c54b20bc764fcbbd384422ce61a9fdb
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2022-04-19 14:28:17 -0500

    created skeleton api and html pages that need to be worked on

---
+++ src/main/java/com/watercooler/apis/MainManagingJobsAPI.java
@@ -0,0 +1,7 @@
+package com.watercooler.apis;
+import io.javalin.Javalin;
+
+public class MainManagingJobsAPI {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/watercooler/apis/ManagingJobsController.java
@@ -0,0 +1,14 @@
+package com.watercooler.apis;
+
+import java.util.logging.Handler;
+
+public class ManagingJobsController {
+
+    //public Handler createJobPost =
+
+    //public Handler viewJobs =
+
+    //public Handler viewApplicants =
+
+    //public Handler deleteJobPost =
+}
